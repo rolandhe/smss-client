59ab2a261e21cc3601b6c0026948df01743e186e
hexiufeng <rolandhe@qq.com>
2024-09-06 14:12:20 +0800

    rm spaces

--- dlock/redis/long_time_lock.go
+++ dlock/redis/long_time_lock.go
@@ -26,12 +26,12 @@ const luaExtendScript = `
     end
 `
 const luaReleaseScript = `
-        if redis.call("get", KEYS[1]) == ARGV[1] then
-            return redis.call("del", KEYS[1])
-        else
-            return 0
-        end
-    `
+	if redis.call("get", KEYS[1]) == ARGV[1] then
+		return redis.call("del", KEYS[1])
+	else
+		return 0
+	end
+`
 
 type redisLocker struct {
 	host string
